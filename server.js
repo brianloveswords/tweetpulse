56618d7dc2191c897b997bf951f4ce3c4f0f84fa
Brian J Brennan <brianloveswords@gmail.com>
2011-03-01 22:57:15 -0500

    Change server to use haml instead of jade.

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 var app = require('express').createServer(),
     sys = require('sys'),
-    jade = require('jade');
+    haml = require('hamljs'),
+    tweetData = require('./data.js');
+
+app.set('view engine', 'hamljs');
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.render('index');
+});
+
+app.get('/users', function(req, res){
+  tweetData.getUsers(function(users){
+    res.render('users', {locals: {users: users }});
+  })
+});
+
+app.get('/tags', function(req, res){
+  tweetData.getTags(function(tags){
+    res.render('users', {locals: {users: tags }});
+  })
 });
 
 sys.puts('listening on port 3000');
 app.listen(3000);
-
