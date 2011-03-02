a9b55616bb81798d33439074565c4f5845d9d3a8
Brian J Brennan <brianloveswords@gmail.com>
2011-03-02 00:53:16 -0500

    User data includes tag/tweet count.

--- server.js
+++ server.js
@@ -14,12 +14,31 @@ app.get('/users', function(req, res){
     return res.render('users', {locals: {users: users }});
   })
 });
+
 app.get('/user/:id', function(req, res){
   var user = req.params.id;
-  tweetData.getUser(user, function(user){
-    return res.render('user', {locals: {user: user }});
+  tweetData.getTags(function(allTags){
+    var plainTags = allTags.map(function(tag){return tag.tag});
+    tweetData.getUser(user, function(user){
+      var userTags = [];
+      user.tweets.forEach(function(tweet){
+        tweet.tags.forEach(function(tag){
+          if (userTags.indexOf(tag) === -1) userTags.push(tag);
+        })
+      })
+      
+      console.dir(userTags);
+      // user -> {screen_name: name, tweets: [...]}
+      return res.render('user', {locals: {user: user , tagsUsed: userTags, tagList: plainTags }});
+    });
   });
 });
+
+/*
+  compare tags.tag to 
+*/
+
+
 
 app.get('/tags', function(req, res){
   tweetData.getTags(function(tags){
